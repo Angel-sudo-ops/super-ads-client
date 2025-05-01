d615fe1bf46325b7584cd104b359037e7eea9d50
Jose Contreras <josecontreras@users.noreply.github.com>
2025-05-01 07:40:04 -0500

    export .exe version 2.4.8.6

--- SuperADSClient.py
+++ SuperADSClient.py
@@ -26,7 +26,7 @@ if not pyads_available:
     # messagebox.showerror("Attention", "No pyads available")
     print("No pyads available")
 
-__version__ = '2.4.8.5'
+__version__ = '2.4.8.6'
 __icon__ = "./plc.ico"
 
 LGV_DATA = "lgv_data.xml"
